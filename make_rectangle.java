4536526880034d61ab9991871d45c0d49cdf08d5
Shivansh Mittal <88429611+Shivansh20128@users.noreply.github.com>
2022-01-06 18:25:30 +0530

    Create make_rectangle.java
    we have 3 sticks. We need to break 1 stick so that it can form a rectangle together with other ones. Takes space-separated input.

---
+++ make_rectangle.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Scratch {
+    public static void main(String[] args) {
+        int l1,l2,l3;
+
+        Scanner scan  = new Scanner(System.in);
+        int testcases = scan.nextInt();
+        int[] array = new int[3*testcases];
+        String[] answers = new String[3*testcases];
+        for(int i=0;i<3*testcases;i+=3){
+            array[i] = scan.nextInt();
+            array[i+1] = scan.nextInt();
+            array[i+2] = scan.nextInt();
+            if(array[i]==array[i+1] && array[i+2]%2==0){
+                answers[i] = "YES";
+            }else if(array[i+1]==array[i+2] && array[i]%2==0){
+                answers[i] = "YES";
+            }else if(array[i]==array[i+2] && array[i+1]%2==0){
+                answers[i] = "YES";
+            }else if(array[i]+array[i+1]==array[i+2] || array[i+1]+array[i+2]==array[i] || array[i]+array[i+2]==array[i+1]){
+                answers[i] = "YES";
+            }else answers[i] = "NO";
+        }
+
+
+
+        for(int i=0;i<3*testcases;i+=3){
+            System.out.println(answers[i]);
+        }
+    }
+}
