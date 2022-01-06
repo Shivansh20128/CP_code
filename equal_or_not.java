2b11a5be9de8c73f87f8aa5c392de2b7e87de172
Shivansh Mittal <88429611+Shivansh20128@users.noreply.github.com>
2022-01-06 22:10:48 +0530

    Create equal_or_not.java
    there is a circular arrangement represented as a string. So u need to tell if an arrangement is possible or not given a string with E's representing that a char is same as the next char and N's representing they are not.

---
+++ equal_or_not.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Scratch {
+    public static void main(String[] args){
+        Scanner scan  = new Scanner(System.in);
+        int testcases = scan.nextInt();
+        String[] size = new String[testcases];
+        String[] ans = new String[testcases];
+        for(int i=0;i<testcases;i++){
+            int e=0;int n=0;
+            size[i] = scan.next();
+            for(int j=0;j<size[i].length();j++){
+                if(size[i].charAt(j)=='E'){
+                    e++;
+                }else if(size[i].charAt(j)=='N'){
+                    n++;
+                }
+                if(n==1){
+                    ans[i]="NO";
+                }else ans[i]="YES";
+            }
+        }
+
+        for(int i=0;i<testcases;i++){
+            System.out.println(ans[i]);
+        }
+
+    }
+}
