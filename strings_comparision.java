1a5f7b0d37abbc43df6a36939cb971e0e8bd1388
Shivansh Mittal <88429611+Shivansh20128@users.noreply.github.com>
2022-01-07 13:07:12 +0530

    Create strings_comparision.java
    Here we need to compare 2 strings of variable lengths where we assume the uppercase and lowercase to be same. if string1 is less than string2, answer is -1, 0 for equal, and 1 otherwise. LEXOGRAPHIC COMPARISION.

---
+++ strings_comparision.java
@@ -0,0 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+class Scratch {
+    static int row;static int column;
+    public static void main(String[] args){
+        Scanner scan  = new Scanner(System.in);
+        String str1 = scan.next();
+        String str2 = scan.next();
+        str1 = str1.toLowerCase(Locale.ROOT);
+        str2 = str2.toLowerCase(Locale.ROOT);
+        int ans = (str1.compareTo(str2));
+        if(ans<0){
+            System.out.println(-1);
+        }else if(ans==0){
+            System.out.println(0);
+        }else if(ans>0){
+            System.out.println(1);
+        }
+
+    }
+}
