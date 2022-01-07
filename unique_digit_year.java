6648b546ac36c9983823a2b33606368e4150e25b
Shivansh Mittal <88429611+Shivansh20128@users.noreply.github.com>
2022-01-07 21:56:51 +0530

    Create unique_digit_year.java
    if an year is given, find a year after that given year having all digits unique. given 1000<=year<=9000.

---
+++ unique_digit_year.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class Scratch {
+    public static boolean isUnique(String str){
+        char a = str.charAt(0);
+        char b = str.charAt(1);
+        char c = str.charAt(2);
+        char d = str.charAt(3);
+        if(a==b || a==c || a==d || b==c || b==d || c==d){
+            return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        while(true) {
+            String year = String.valueOf(n+1);
+            if (isUnique(year)) {
+                System.out.println(year);
+                break;
+            } else {
+                n++;
+            };
+        }
+
+
+    }
+}
